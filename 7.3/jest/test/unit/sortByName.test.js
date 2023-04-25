d784531e271cd0ee0df57257f063b8400ddfbbdb
Дарья Фокина <cool.russia-33@yandex.ru>
2023-04-25 15:09:36 +0300

    added tests coverage

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,21 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [
+      "Властелин Колец",
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+    
+    const result = sorting.sortByName(input);
+    expect(result).toEqual(expected);
   });
 });
