92d9bf916bab4e9b5f67a3591a214b91b4b530f9
Дарья Фокина <cool.russia-33@yandex.ru>
2023-04-26 19:27:46 +0300

    added tests

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,21 +2,25 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
   page.close();
 });
 
-describe("Github page tests", () => {
-  test("The h1 header content'", async () => {
+describe("Github page tests", () => { 
+  
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
+  test("The h1 header content", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+    expect(title2).toEqual('GitHub: Let’s build from here · GitHub');
+  }, 60000);
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", link => link.getAttribute('href') );
@@ -29,6 +33,36 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    expect(actual).toContain("Get started with Team")
   });
+});
+
+describe("Another page tests", () => { 
+
+  test("The first h1 test", async () => {
+    await page.goto("https://github.com");
+    const firstLink = await page.$("a.home-campaign-enterprise");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Choose an Enterprise plan · GitHub');
+  }, 40000);
+
+  test("The second h1 test", async () => {
+    await page.goto("https://github.com/signup?user_email=&source=form-home-signup");
+    const firstLink = await page.$("a.color-fg-on-emphasis:nth-child(2)");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Sign in to GitHub · GitHub');
+  }, 60000);
+
+  test("The third h1 test", async () => {
+    await page.goto("https://github.com/features/actions");
+    const firstLink = await page.$("main div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.$eval("main h1", link => link.textContent);
+    expect(title2).toEqual('The tools you need to build what you want.');
+  }, 40000);
 });
